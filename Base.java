a213b8f4bfd29b6519851ed441a76cd6d7ca2dda
Stanislau Bazarnou <Stanislau_Bazarnou@epam.com>
2024-04-24 20:09:33 +0400

    Added implementation for subtraction, division, multiplication, and square root methods
    Added dix method to Base.java
    
    Added mult method to Base.java
    
    Return sqrt method to Base.java file

--- Base.java
+++ Base.java
@@ -2,4 +2,26 @@ public class Base {
     public int add(int a, int b) {
         return a + b;
     }
+
+    public int sub(int a, int b) {
+	return a - b;
+    }
+
+    public int div(int a, int b) {
+    if (b == 0) {
+        throw new IllegalArgumentException("Cannot divide by zero");
+      }
+    return a / b;
+    }
+
+    public int mult(int a, int b) {
+	return a * b;
+    }
+
+    public double sqrt(int a) {
+    if (a < 0) {
+        throw new IllegalArgumentException("Cannot calculate square root of a negative number");
+  	}
+  	return Math.sqrt(a);
+    }
 }
