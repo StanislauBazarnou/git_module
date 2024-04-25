9c73d9e49b25c8d75e5cd5905a5c2d2de2b54ada
Stanislau Bazarnou <Stanislau_Bazarnou@epam.com>
2024-04-25 17:16:38 +0400

    Added add method with 3 parameters

--- Base.java
+++ Base.java
@@ -8,4 +8,8 @@ public class Base {
         return a + b;
     }
 
+    public int add(int a, int b, int c) {
+        System.out.println("This is add method for 3 params");
+        return a + b + c;
+    }
 }
