ad953c659e436f03a0940de236d13a67350eecde
Stanislau Bazarnou <Stanislau_Bazarnou@epam.com>
2024-04-25 17:14:32 +0400

    Added add method with 2 parameters

--- Base.java
+++ Base.java
@@ -2,4 +2,10 @@ public class Base {
     public int add(int a, int b) {
         return a + b;
     }
+
+    public int add(int a, int b) {
+        System.out.println("This is add method for 2 params");
+        return a + b;
+    }
+
 }
